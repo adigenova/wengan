49f931dd77e019e85495602d9b326f1449ed1465
adigenova <digenova@gmail.com>
2019-06-02 17:20:51 +0200

    first complete version of the wengan pipeline

--- perl/Wengan/Bruijn/Disco.pm
+++ perl/Wengan/Bruijn/Disco.pm
@@ -34,7 +34,7 @@ sub create_jobs{
   #minia contigs were create previously
   if(defined $self->{contigs}){
       my $job=();
-      push(@{$job->{target}},$self->{prefix}.".disco.fa");
+      push(@{$job->{target}},$self->{prefix}.".contigs-disco.fa");
       my $cmd="ln -s $self->{contigs} $job->{target}[0]";
       push(@{$job->{cmds}},$cmd);
       push(@{$self->{jobs}},$job);
@@ -42,60 +42,59 @@ sub create_jobs{
       #clean the contigs
       $self->_create_seqtk_jobs(@{$self->{jobs}}[$#{$self->{jobs}}]->{target}[0]);
       push(@{$self->{main_target}},@{$self->{jobs}}[$#{$self->{jobs}}]->{target}[0]);
-
   }else{
     #default method for create jobs
     $self->_create_jobs_short($reads);
+    push(@{$self->{main_target}},@{$self->{jobs}}[$#{$self->{jobs}}]->{target}[0]);
   }
 
 }
-
 sub _def_parameters{
       my ($self,$rlen)=@_;
       #Disco configuration used on the wengan ms
-      #${TIME} -v -o time_Disco_ILL150bp60X.txt Disco-pe  name=HG00733-Disco-ILL60X250
-      #np=20 k=128
-      #B=40G H=4 kc=3 v=-v contigs 2>Disco60X.err >Disco60X.log
-      #lib="pea peb" pea="SRR5534476_1.fastq.gz SRR5534476_2.fastq.gz" peb="SRR5534475_1.fastq.gz SRR5534475_2.fastq.gz"
-      my $param="k=96  B=40G H=4 kc=3 v=-v ";
-      if($rlen >=200){
-        $param="k=128 B=40G H=4 kc=3 v=-v ";
-      }
+      # we use the discovar varsion discovarexp-51885
+      # available on  ftp://ftp.broadinstitute.org/pub/crd/nightly/discovardenovo/discovarexp/discovarexp-51885.tar.gz
+      #export MALLOC_PER_THREAD=1
+      #${DISCO} READS="SRR891258_{1,2}.fastq.gz,SRR891259_{1,2}.fastq.gz" NUM_THREADS=44 OUT_DIR=/tmp/60XNA12878
+      #cp -ra /tmp/60XNA12878 .
+      #
+
+      my $param="NUM_THREADS=$self->{cores} OUT_DIR=/tmp/$self->{prefix}D";
       return $param;
 }
 
 sub _create_jobs_short{
     my ($self,$reads)=@_;
     #we get the parameters for Disco2
-    my $params = $self->_def_parameters($reads->{sreads}[0]->{len});
+    #my $params = $self->_def_parameters($reads->{sreads}[0]->{len});
     #preparing the reads for Disco2
-    my $l=1;
-    my @libs=();
-    my @lnames=();
+    my @rfiles=();
     foreach my $r (@{$reads->{sreads}}){
-            my $lib="pe$l=\"$r->{fwd} $r->{rev}\"";
-            push(@libs,$lib);
-            push(@lnames,"pe$l");
-            $l++;
+            push(@rfiles,"$r->{fwd},$r->{rev}");
     }
+    my $readfiles=join(",",@rfiles);
     #now we create the asbyss cmd
-    my @mopt=("name=".$self->{prefix}.".Disco2",
-      $params,
-      "np=$self->{cores}",
-      "lib=\"".join(" ",@lnames)."\"",
-      join(" ",@libs),
-      "contigs",
-      "2>",$self->{prefix}.".Disco2.err",
-      ">",$self->{prefix}.".Disco2.log");
+    my @mopt=(
+      "READS=\"$readfiles\"",
+      "OUT_DIR=/tmp/$self->{prefix}D",
+      "NUM_THREADS=$self->{cores}",
+      "2>",$self->{prefix}.".Disco_denovo.err",
+      ">",$self->{prefix}.".Disco_denovo.log");
       my $job=();
-      #the Disco2 contigs
-      push(@{$job->{target}},$self->{prefix}.".Disco2-contigs.fa");
-      push(@{$job->{cmds}},join(" ",Disco2_BIN,@mopt));
+      #We run the disco program
+      my $cmd="export MALLOC_PER_THREAD=1";
+      push(@{$job->{cmds}},$cmd);
+      push(@{$job->{cmds}},join(" ",DiscoVarDenovo_BIN,@mopt));
+      $cmd="cp -a /tmp/$self->{prefix}D $self->{prefix}D";
+      push(@{$job->{cmds}},$cmd);
+      $cmd="ln -s $self->{prefix}D/a.final/a.lines.fasta $self->{prefix}.contigs-disco.fa";
+      push(@{$job->{cmds}},$cmd);
+
+      push(@{$job->{target}},$self->{prefix}.".contigs-disco.fa");
       push(@{$self->{jobs}},$job);
     # we add the after assembly steps
     $self->_create_seqtk_jobs(@{$self->{jobs}}[$#{$self->{jobs}}]->{target}[0]);
     push(@{$self->{main_target}},@{$self->{jobs}}[$#{$self->{jobs}}]->{target}[0]);
-
     #return $self->{jobs};
 }
 
@@ -104,7 +103,7 @@ sub _create_seqtk_jobs{
   my $job=();
   push(@{$job->{deps}},$contigs);
   my $target=$contigs;
-  $target=~s/.Disco2-contigs.fa/.Disco2.contigs.fa/;
+  $target=~s/.contigs-disco.fa/.contigs.disco.fa/;
   push(@{$job->{target}},$target);
   #SEQTK options to clean the Disco result file
   #${SEQTK} cutN -n 1 $(patsubst %.splitN.fa,%.fa,$@)  |
@@ -115,7 +114,6 @@ sub _create_seqtk_jobs{
      $cmd.=SEQTK_BIN." iupac2bases -  | ";
      $cmd.=SEQTK_BIN." rename - D | ";
      $cmd.=SEQTK_BIN." seq -l 60 - > $target";
-
   push(@{$job->{cmds}},$cmd);
   push(@{$self->{jobs}},$job);
 }
